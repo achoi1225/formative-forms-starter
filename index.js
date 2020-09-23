2d2a60f2d51503a434ec9c57cf83d28a4e6cb4f9
ryan <ryan.d.matuszak@gmail.com>
2020-09-23 13:06:03 -0500

    created csuf protection

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
 const express = require("express");
+const cookieParser = require("cookie-parser");
+const csrf = require("csurf");
+
+
+const csrfProtection = csrf({cookie: true});
 
 const app = express();
+
+app.use(cookieParser());
+app.use(express.urlencoded({extended: true}));
+
 const port = process.env.PORT || 3000;
 
+
+
+
 app.set("view engine", "pug");
+
 
 app.get("/", (req, res) => {
   res.render("index", { users: users });
 });
+
+app.get("/create", csrfProtection, (req, res) => {
+  res.render("create-normal", {csrfToken: req.csrfToken()})
+
+})
+
+
+app.get("/create-interesting", csrfProtection, (req, res) => {
+  res.render("create-interesting", {csrfToken: req.csrfToken()})
+})
 
 const users = [
   {
