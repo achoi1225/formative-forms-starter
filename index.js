39f3c293f65196d2182faeb8fb8a7056e6b09742
ryan <ryan.d.matuszak@gmail.com>
2020-09-23 16:00:44 -0500

    finished phase 3

--- index.js
+++ index.js
@@ -14,6 +14,37 @@ const port = process.env.PORT || 3000;
 
 app.set("view engine", "pug");
 
+const users = [
+  {
+    id: 1,
+    firstName: "Jill",
+    lastName: "Jack",
+    email: "jill.jack@gmail.com"
+  }
+];
+
+const validateUser = (req, res, next) => {
+  const { id, firstName, lastName, email, password, confirm_password } = req.body;
+  const errors = [];
+  if(!firstName) {
+    errors.push("Please fill out the first name field")
+  };
+  if(!lastName) {
+    errors.push("Please fill out the last name field")
+  };
+  if(!email) {
+    errors.push("Please fill out the email field")
+  };
+  if(!password) {
+    errors.push("Please fill out the password field")
+  };
+  if(password !== confirm_password) {
+    errors.push("Passwords must match")
+  }
+  req.errors = errors;
+  next();
+}
+
 
 app.get("/", (req, res) => {
   res.render("index", { users: users });
@@ -29,14 +60,29 @@ app.get("/create-interesting", csrfProtection, (req, res) => {
   res.render("create-interesting", {csrfToken: req.csrfToken()})
 })
 
-const users = [
-  {
-    id: 1,
-    firstName: "Jill",
-    lastName: "Jack",
-    email: "jill.jack@gmail.com"
+app.post("/create", validateUser, (req, res) => {
+  const { id, firstName, lastName, email } = req.body;
+  if(req.errors.length > 0) {
+    res.render("create-normal", {
+      errors: req.errors,
+      id,
+      firstName,
+      lastName,
+      email
+    })
+    return;
   }
-];
+  const user = {
+    id,
+    firstName,
+    lastName,
+    email
+  };
+  users.push(user);
+  res.redirect("/");
+})
+
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
